0d4ef8964b8664678a080130e8ac221c72ef6cb0
Bytyci <renis.bytyci@fiserv.com>
2023-11-13 15:12:45 -0500

    new branch

--- H2database/src/main/resources/schema.sql
+++ H2database/src/main/resources/schema.sql
@@ -4,3 +4,14 @@ name VARCHAR(45),
 price DOUBLE,
 serving VARCHAR(45),
 PRIMARY KEY (id));
+
+CREATE TABLE if NOT EXISTS zip(
+    zip INT NOT NULL AUTO_INCREMENT,
+    city VARCHAR(45),
+    state VARCHAR(45),
+    PRIMARY KEY (zip));
+
+INSERT INTO zip VALUES (55501, 'Minnetonka', 'Minnesota');
+INSERT INTO zip VALUES (55502, 'Moorhead', 'Minnesota');
+INSERT INTO zip VALUES (55503, 'Wayzata', 'Minnesota');
+INSERT INTO zip VALUES (55504, 'New Ulm', 'Minnesota');
