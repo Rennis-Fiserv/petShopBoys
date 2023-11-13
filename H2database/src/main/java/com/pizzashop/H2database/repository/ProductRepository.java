ca34d253c700b4a122737840ab2ae160b05bcc45
Drew-Huth <147175635+Drew-Huth@users.noreply.github.com>
2023-11-13 21:25:01 +0000

    updated entities

--- H2database/src/main/java/com/pizzashop/H2database/repository/ProductRepository.java
+++ H2database/src/main/java/com/pizzashop/H2database/repository/ProductRepository.java
@@ -8,5 +8,5 @@ import com.pizzashop.H2database.model.Product;
 public interface ProductRepository extends R2dbcRepository<Product, Integer> {
     Flux<Product> findByName(String name);
 
-    Flux<Product> findByPublished(boolean isPublished);
+    // Flux<Product> findByPublished(boolean isPublished);
 }
--- H2database/src/test/java/com/pizzashop/H2database/H2databaseApplicationTests.java
+++ H2database/src/test/java/com/pizzashop/H2database/H2databaseApplicationTests.java
@@ -1,13 +1,13 @@
-package com.pizzashop.H2database;
+// package com.pizzashop.H2database;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class H2databaseApplicationTests {
+// @SpringBootTest
+// class H2databaseApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+// 	@Test
+// 	void contextLoads() {
+// 	}
 
-}
+// }
